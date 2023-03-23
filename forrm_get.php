ea8c7dc18a4def42c2a283e22e66d1e9b7fc74d9
Dusty Shaw <shustydaw@gmail.com>
2023-03-23 11:30:48 -0600

    Trying a new form

--- forrm_get.php
+++ forrm_get.php
@@ -1,5 +1,20 @@
 <html>
     <body>
-        Thanks for submitting,  <?php echo $_GET["name"]; ?>!<br>
+        <!-- Thanks for submitting,  <?php echo $_GET["name"]; ?>!<br> -->
+
+        <?php
+            $to = "shustydaw@gmail.com";
+            $subject = "Portfolio Message";
+
+            $header = "From: ";
+
+            $retval = mail ($to,$subject,$message,$header);
+
+            if ($retval == true) {
+                echo "Message send successfully!";
+            } else {
+                echo "Something went wrong...";
+            }
+        ?>
     </body>
 </html>
