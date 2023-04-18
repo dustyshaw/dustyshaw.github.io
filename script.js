76608b0b37df294db192e1a47b35532d2cd96995
Dusty Shaw <shustydaw@gmail.com>
2023-04-18 10:05:41 -0600

    changed file name

--- script.js
+++ script.js
@@ -2,17 +2,13 @@ let changeThemeBtn = document.getElementById("changeThemeBtn");
 let darkTheme = false;
 
 changeThemeBtn.addEventListener("click", function () {
-    console.log("button clicked");
-    // document.querySelectorAll("body").style.backgroundColor = "black";
     if (darkTheme == false) {
         document.body.style.backgroundColor = "black";
-    document.body.style.color = "#4AF626";
+        document.body.style.color = "#4AF626";
         darkTheme = true;
     } else {
         document.body.style.backgroundColor = "white";
         document.body.style.color = "black";
         darkTheme = false;
     }
-    
-
 });
