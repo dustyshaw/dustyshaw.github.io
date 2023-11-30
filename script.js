f9a0a76c68e8c859bad03b912d104a067ab43c08
Dusty Shaw <shustydaw@gmail.com>
2023-11-30 16:19:20 -0700

    Updating Styles

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let changeThemeBtn = document.getElementById("changeThemeBtn");
 let darkTheme = false;
 
+const grayRamp = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\'. ';
+
 changeThemeBtn.addEventListener("click", function () {
     if (darkTheme == false) {
         document.body.style.backgroundColor = "black";
@@ -12,3 +14,16 @@ changeThemeBtn.addEventListener("click", function () {
         darkTheme = false;
     }
 });
+
+// Function that fades in sections when they are in view of user
+const observer = new intersectionCallback((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('fade-show');
+        }
+    });
+  });
+  
+const hiddenele = document.querySelectorAll(".fade-hidden");
+hiddenele.forEach((ele) => observer.observe(ele));
