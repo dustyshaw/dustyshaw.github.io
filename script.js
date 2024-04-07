b2c35c21cb48c0da16161718fa69f925214ba699
shust <shust@DESKTOP-6GNUVDA>
2024-04-07 16:11:34 -0600

    creating more content

--- script.js
+++ script.js
@@ -1,43 +1,30 @@
-let changeThemeBtn = document.getElementById("changeThemeBtn");
-let darkTheme = false;
+// let changeThemeBtn = document.getElementById("changeThemeBtn");
+// let darkTheme = false;
 
-const grayRamp = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\'. ';
+// const grayRamp = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/|()1{}[]?-_+~<>i!lI;:,"^`\'. ';
 
-changeThemeBtn.addEventListener("click", function () {
-    if (darkTheme == false) {
-        document.body.style.backgroundColor = "black";
-        document.body.style.color = "#4AF626";
-        darkTheme = true;
-    } else {
-        document.body.style.backgroundColor = "white";
-        document.body.style.color = "black";
-        darkTheme = false;
-    }
-});
+// changeThemeBtn.addEventListener("click", function () {
+//     if (darkTheme == false) {
+//         document.body.style.backgroundColor = "#2e303c";
+//         document.body.style.color = "#4AF626";
+//         darkTheme = true;
+//     } else {
+//         document.body.style.backgroundColor = "white";
+//         document.body.style.color = "black";
+//         darkTheme = false;
+//     }
+// });
 
 // Function that fades in sections when they are in view of user
-const observer = new intersectionCallback((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('fade-show');
-        }
-    });
-  });
+// const observer = new intersectionCallback((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('fade-show');
+//         }
+//     });
+//   });
   
-const hiddenele = document.querySelectorAll(".fade-hidden");
-hiddenele.forEach((ele) => observer.observe(ele));
+// const hiddenele = document.querySelectorAll(".fade-hidden");
+// hiddenele.forEach((ele) => observer.observe(ele));
 
-
-function obfuscateEmail(email) {
-    var obfuscatedEmail = "";
-    for (var i = 0; i < email.length; i++) {
-        obfuscatedEmail += "&#" + email.charCodeAt(i) + ";";
-    }
-    return obfuscatedEmail;
-}
-
-var email = "shustydaw@gmail.com";
-var obfuscatedEmail = obfuscateEmail(email);
-document.getElementById("obfuscated-email").innerHTML = obfuscatedEmail;
- 
